11acc93a4f3d6d1530546b16788547320626dc24
David Harding 💲 <davhar@ntdev.microsoft.com>
2022-01-24 06:20:46 -0800

    Latest changes

--- EntitlementsToken/contracts/EntitlementTokenV1.sol
+++ EntitlementsToken/contracts/EntitlementTokenV1.sol
@@ -1,73 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Ownable.sol";
 
-contract EntitlementTokenV1 is ERC721, ERC721Enumerable, Ownable {
-    constructor() ERC721("EntitlementTokenV1", "ET") {}
-
+contract Entitlements is Ownable {
+    
     enum EntitlementState {
         ACTIVE,
         INACTIVE,
         EXPIRED
     }
 
-    struct EntitlementInfo {
+    struct EntitlementData {
         uint256 registeredOn;
         EntitlementState state;
         string productId;
     }
 
-    // List of all the entitlements tokens issued
-    EntitlementInfo[] _tokens;
+    mapping(string => mapping(string => EntitlementData)) _users;
 
     function grant(
-        address account,
-        string memory productId) public onlyOwner returns (uint256) {
-        require(account != address(0), "mint to the zero address");
+        string memory puid,
+        string memory productId) public onlyOwner () {
 
         // create new token
-        EntitlementInfo memory token = EntitlementInfo({
+        EntitlementData memory entitlement = EntitlementData({
             state: EntitlementState.ACTIVE,
             registeredOn: block.timestamp,
             productId: productId
         });
 
-        _tokens.push(token);
-        uint256 tokenId = _tokens.length - 1;
-
-        _safeMint(account, tokenId);
-        return tokenId;
+        _users[puid][productId] = entitlement;
     }
 
     function revoke(
-        address account, 
-        uint256 _tokenId) public onlyOwner {
-        require(account != address(0), "mint to the zero address");
+        string memory puid,
+        string memory productId) public onlyOwner () {
 
-        EntitlementInfo storage token = _tokens[_tokenId];
-        require(token.registeredOn != 0);
-        require(token.state == EntitlementState.ACTIVE);
-
-        token.state = EntitlementState.INACTIVE;
-    }
-    
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+        _users[puid][productId].state = EntitlementState.INACTIVE;
     }
 
-    // PRIVATE
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) 
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
+    function state(
+        string memory puid,
+        string memory productId) public view returns (EntitlementState) {
+
+        return _users[puid][productId].state;
     }
 }
--- EntitlementsToken/test/EntitlementTokenV1.test.js
+++ EntitlementsToken/test/EntitlementTokenV1.test.js
@@ -1,65 +1,31 @@
 const { expect } = require("chai");
 
-describe("EntitlementTokenV1 contract", function () {
+describe("Entitlements contract", function () {
   
-  let Token;
-  let hardhatToken;
-  let owner;
-  let addr0;
-  let addr1;
-  let addr2;
-  let addrs;
+  let contract;
+  let instance;
 
   // `beforeEach` will run before each test, re-deploying the contract every
   // time. It receives a callback, which can be async.
   beforeEach(async function () {
 
-    Token = await ethers.getContractFactory("EntitlementTokenV1");
-    addr0 = ethers.constants.AddressZero;
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();   
-
-    hardhatToken = await Token.deploy();
-  });
-
-  describe("Deployment", function () {
-    it("Should assign the total supply of tokens to the owner", async function () {    
-
-      console.log("owner", owner.address);
-      console.log("addr1", addr1.address);
-      console.log("addr2", addr2.address);
-
-      const ownerBalance = await hardhatToken.balanceOf(owner.address);
-      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-    });
+    contract = await ethers.getContractFactory("Entitlements");
+    instance = await contract.deploy();
   });
 
   describe("Transactions", function () {
+
     it("Can grant entitlement", async function() {     
 
-      await expect(hardhatToken.grant(addr1.address, "12345"))
-        .to.emit(hardhatToken, "Transfer")
-        .withArgs(addr0, addr1.address, 0);    
-
-      let balance = await hardhatToken.balanceOf(addr1.address);
-      expect(balance).to.equal(1);
-
-      //let ownerOf = await hardhatToken.ownerOf(0);
-      //expect(ownerOf.address).to.equal(addr1.address);
+      await instance.grant("1", "12345");
+      expect(await instance.state("1", "12345")).to.equal(0);
     });
 
     it("Can revoke entitlement", async function() {     
 
-      await expect(hardhatToken.grant(addr1.address, "12345"))
-        .to.emit(hardhatToken, "Transfer")
-        .withArgs(addr0, addr1.address, 0);    
-
-      await hardhatToken.revoke(addr1.address, tokenId);
-
-      let balance = await hardhatToken.balanceOf(addr1.address);
-      expect(balance).to.equal(1);
-
-      //let ownerOf = await hardhatToken.ownerOf(0);
-      //expect(ownerOf.address).to.equal(addr1.address);
+      await instance.revoke("1", "12345");
+      expect(await instance.state("1", "12345")).to.equal(1);
     });
+
   });
 });
